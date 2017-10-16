0e52dc280f4e0e0aa7922a1e52911915e4d5de81
I432 <zaxs0930412360@gamil.com>
2017-10-16 15:06:04 +0800

    62+

---
+++ src/hljkghv.java
@@ -0,0 +1,16 @@
+public class hljkghv {
+
+    public static void main(String[] args) {
+        //顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        final double PI=3.1415926;
+
+
+        float r=sc.nextFloat();
+        float h=sc.nextFloat();
+
+        System.out.println(PI*Math.pow(r,2)*h);
+
+    }
+}
